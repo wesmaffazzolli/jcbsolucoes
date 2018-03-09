880ba1f1f5d5daf454e74f67c1144687bac71d86
wesmaffazzolli <31376480+wesmaffazzolli@users.noreply.github.com>
2018-03-09 17:40:45 -0300

    Sb-admin basic version

---
+++ admin/index.php
@@ -0,0 +1,23 @@
+<?php include "includes/admin_header.php"; ?>
+   
+    <div id="wrapper">
+
+    <?php include "includes/admin_navigation.php"; ?>
+
+    <div id="page-wrapper">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Dashboard</h1>
+            </div>
+            <!-- /.col-lg-12 -->
+            
+        </div>
+        <!-- /.row -->
+        
+    </div>
+    <!-- /#page-wrapper -->
+
+</div>
+<!-- /#wrapper -->
+
+<?php include "includes/admin_footer.php"; ?>
